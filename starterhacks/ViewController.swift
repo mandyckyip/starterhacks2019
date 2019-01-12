b9a22f7fa6aacbd8f7b9b63d97da8bdb94f683bd
cumar221 <omar221@windowslive.com>
2019-01-12 18:04:04 -0500

    first commit

---
+++ starterhacks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starterhacks
+//
+//  Created by Cumar Yusuf on 2019-01-12.
+//  Copyright © 2019 Cumar Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
