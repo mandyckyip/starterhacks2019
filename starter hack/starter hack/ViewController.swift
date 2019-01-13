c72fbd0ae32d5347bd00e76164e34a049cdefcbf
Mandy Yip <45083183+mandyckyip@users.noreply.github.com>
2019-01-13 13:24:14 -0500

    Add files via upload

---
+++ starter hack/starter hack/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  starter hack
+//
+//  Created by Mandy Yip on 2019-01-12.
+//  Copyright © 2019 Mandy Yip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func button(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
